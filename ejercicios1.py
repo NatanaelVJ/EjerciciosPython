74e58d6ad6c66fd2ca0c9d1e74f6388ee0559d46
NatanaelVJ <natanael.villalon@gmail.com>
2022-08-30 17:54:10 -0400

    ejercicios

---
+++ ejercicios1.py
@@ -0,0 +1,17 @@
+
+
+Resultado = ((3+2)/(2*5))**2
+
+print("************ 1 **************")
+
+print("El resultado es:", Resultado)
+
+print("************* 2 **************")
+
+payaso = 112
+muñeca = 75
+venta = payaso*23+muñeca*54
+print("El peso total de la venta es:", venta)
+
+print("***************************")
+
